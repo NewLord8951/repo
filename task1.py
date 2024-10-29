c677e8beacc828cc7521baf98f3c43afaf48f11e
New_Lord_8951 <alexandrmanorali@gmail.com>
2024-10-29 09:53:37 +0300

    Я сделал 3-ю задачу

---
+++ task1.py
@@ -0,0 +1,13 @@
+from math import pi
+
+print(round(pi, 2))
+
+from math import e
+
+print(round(e, 1))
+
+number = int(input("Введите число"))
+print("Вы ввели данное число")
+
+number = int(input(""))
+print("вот какое число Вы ввели")
